085f985b4dc34654ffe8bb5036b77b9363b75ef4
Jean Costa <35783663+jcp10@users.noreply.github.com>
2024-01-17 19:53:01 -0300

    psycopg2

--- fakePinterest/__init__.py
+++ fakePinterest/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 #criando a aplicação
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///comunidade.db'
+app.config['SQLALCHEMY_DATABASE_URI']= os.getenv('DATABASE_URL') 
 app.config['SECRET_KEY']='225a184abe15673d7f2a9c6bfc5eb8b6'
 app.config['UPLOAD_FOLDER']='static/fotos_posts'
 
